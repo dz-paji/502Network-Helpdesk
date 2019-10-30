69aaeed157a073b888f0c0b24ee9bcefe9f82a00
dz_paji <zhangqian2002@outlook.com>
2019-10-30 22:34:05 +0800

    create readme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,12 +6,18 @@ module.exports = {
     repo: 'dz-apji/502Network-knowledge-base',
     sidebar: [
       '/',
-      '/welcome',
       '/test'
     ],
     nav: [{
       text: 'Admin',
       link: '/admin/#/',
     }]
+  }
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+    }
   }
 }
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+---
+title: 欢迎来到 502Network 知识库
+home: true
+lang: zh-CN
+---
+
+# 欢迎
+
+Welcome aboard. 你可以在侧栏中找到更多文章。
